82c4ae8f0ad9a6e0f25e910a73463cc71d5a731a
Pablo Blasco <pabloblascof@gmail.com>
2018-03-16 16:34:20 +0100

    Initial version

---
+++ openfda/newfile.py
@@ -0,0 +1,10 @@
+a = 1
+b = 0
+
+print ("Hello from Pycharm 1")
+print ("Hello from Pycharm 2")
+print ("Hello from Pycharm 3")
+print ("Hello from Pycharm 4")
+print ("Hello from Pycharm 5")
+print ("Hello from Pycharm 6")
+print ("Hello from Pycharm 7")
