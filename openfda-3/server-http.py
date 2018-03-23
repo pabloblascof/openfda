e7dac97b751c65425c140e680bfa229f14669898
Pablo Blasco <pabloblascof@gmail.com>
2018-03-23 16:58:43 +0100

    First version

--- openfda-3/server-http.py
+++ openfda-3/server-http.py
@@ -1,5 +1,7 @@
 import http.server
 import socketserver
+import http.client
+import json
 
 # -- IP and the port of the server
 IP = "localhost"  # Localhost means "I": your local machine
@@ -12,19 +14,29 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         # Send response status code
         self.send_response(200)
-        request_msg = self.path.replace("\r", "").split("\n")
-        request_line = request_msg[0]
-        request = request_line.split(" ")
 
-        req_cmd = request[0]
-        path = request[1]
-
+        # Send headers
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        
-        message = "Hello world! " + self.path
 
-        self.wfile.write(bytes(message, "utf8"))
+        
+        
+        headers = {'User-Agent': 'http-client'}
+
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        conn.request("GET", "/drug/label.json?limit=10", None, headers)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        repos_raw = r1.read().decode("utf-8")
+        conn.close()
+
+        repos = json.loads(repos_raw)
+
+        for elements in repos["results"]:
+            message = (elements['id'] + '\n')
+            # Send message back to client
+            # Write content as utf-8 data
+            self.wfile.write(bytes(message, "utf8"))
         print("File served!")
         return
 
@@ -34,6 +46,14 @@ Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer((IP, PORT), Handler)
 print("serving at port", PORT)
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+        pass
+
+httpd.server_close()
+print("")
+print("Server stopped!")
+
 
 # https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
